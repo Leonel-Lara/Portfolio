5ad30dc47282d6c4cb1395f8b676bdc7e42e9056
Leonel Lara da Silva <leonel.silva.14@hotmail.com>
2021-04-28 21:32:29 -0300

    :up: :package: Aba conhecimentos

--- script.js
+++ script.js
@@ -1,4 +1,89 @@
-document.querySelector(".hamburguer").addEventListener("click", () =>
+document
+  .querySelector(".hamburguer")
+  .addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-);
+  );
 
+document
+  .querySelector(".a1")
+  .addEventListener(
+    "mouseout",
+    () => (document.querySelector(".text1").style.display = "none")
+  );
+
+document
+  .querySelector(".a1")
+  .addEventListener(
+    "mouseover",
+    () => (document.querySelector(".text1").style.display = "")
+  );
+
+document
+  .querySelector(".a2")
+  .addEventListener(
+    "mouseout",
+    () => (document.querySelector(".text2").style.display = "none")
+  );
+
+document
+  .querySelector(".a2")
+  .addEventListener(
+    "mouseover",
+    () => (document.querySelector(".text2").style.display = "")
+  );
+
+document
+  .querySelector(".a3")
+  .addEventListener(
+    "mouseout",
+    () => (document.querySelector(".text3").style.display = "none")
+  );
+
+document
+  .querySelector(".a3")
+  .addEventListener(
+    "mouseover",
+    () => (document.querySelector(".text3").style.display = "")
+  );
+
+document
+  .querySelector(".a4")
+  .addEventListener(
+    "mouseout",
+    () => (document.querySelector(".text4").style.display = "none")
+  );
+
+document
+  .querySelector(".a4")
+  .addEventListener(
+    "mouseover",
+    () => (document.querySelector(".text4").style.display = "")
+  );
+
+document
+  .querySelector(".a5")
+  .addEventListener(
+    "mouseout",
+    () => (document.querySelector(".text5").style.display = "none")
+  );
+
+document
+  .querySelector(".a5")
+  .addEventListener(
+    "mouseover",
+    () => (document.querySelector(".text5").style.display = "")
+  );
+
+document
+  .querySelector(".a6")
+  .addEventListener(
+    "mouseout",
+    () => (document.querySelector(".text6").style.display = "none")
+  );
+
+document
+  .querySelector(".a6")
+  .addEventListener(
+    "mouseover",
+    () => (document.querySelector(".text6").style.display = "")
+  );
